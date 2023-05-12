b9712281e6e1897b43aa65fde29e2b2a6058f2bb
Roman Akhmadullin <akhmadullin_roman@mail.ru>
2023-05-12 11:01:57 +1000

    Fix text

--- src/app/view/components/footer/index.tsx
+++ src/app/view/components/footer/index.tsx
@@ -11,7 +11,7 @@ const Footer: FC = () => {
 
                 <img src={logo} className="footer__logo" alt="Майский гром" />
                 <div className="footer__content__text">
-                    <p>Первенство города Владивостока по спортивному туризму</p>
+                    <p>Первенство города Владивостока по&nbsp;спортивному&nbsp;туризму</p>
                     <p>13 мая 2023 г.</p>
                     <p>Почта для связи - <a href="mailto:club-vibram@mail.ru">club-vibram@mail.ru</a></p>
                 </div>
