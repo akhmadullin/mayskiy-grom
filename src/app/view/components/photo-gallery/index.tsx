1229c9b4de899ff4f1e40c48c18bcc2605cc9330
Roman Akhmadullin <akhmadullin_roman@mail.ru>
2023-05-14 13:58:06 +1000

    Add target blank to disk link

--- src/app/view/components/photo-gallery/index.tsx
+++ src/app/view/components/photo-gallery/index.tsx
@@ -10,7 +10,7 @@ const PhotoGallerySection: FC<PhotoGallerySectionProps> = ({ id }) => {
         <section id={id} className="photo-gallery-section">
             <div className="content-wrapper">
                 <h2 className="photo-gallery-section__title">Фотоотчет</h2>
-                <p className="photo-gallery-section__subtitle">Больше фотографий на&nbsp;<a href="https://disk.yandex.ru/d/VIwictnaue_D_g">Яндекс диске</a></p>
+                <p className="photo-gallery-section__subtitle">Больше фотографий на&nbsp;<a href="https://disk.yandex.ru/d/VIwictnaue_D_g" target='_blank'>Яндекс диске</a></p>
                 <PhotoGallery />
             </div>
         </section>
