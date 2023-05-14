706649e92c27434e673a67e38e328e12927ebbf1
Roman Akhmadullin <akhmadullin_roman@mail.ru>
2023-05-14 13:30:54 +1000

    try to fix image import in ci

--- src/app/view/components/photo-gallery/photos-list.ts
+++ src/app/view/components/photo-gallery/photos-list.ts
@@ -1,130 +1,130 @@
-import o1 from '../../../../assets/photos/big/1.jpeg';
-import t1 from '../../../../assets/photos/thumbnails/1.jpeg';
-import t2x1 from '../../../../assets/photos/thumbnails2x/1.jpeg';
+import o1 from 'url:../../../../assets/photos/big/1.jpeg';
+import t1 from 'url:../../../../assets/photos/thumbnails/1.jpeg';
+import t2x1 from 'url:../../../../assets/photos/thumbnails2x/1.jpeg';
 
-import o2 from '../../../../assets/photos/big/2.jpeg';
-import t2 from '../../../../assets/photos/thumbnails/2.jpeg';
-import t2x2 from '../../../../assets/photos/thumbnails2x/2.jpeg';
+import o2 from 'url:../../../../assets/photos/big/2.jpeg';
+import t2 from 'url:../../../../assets/photos/thumbnails/2.jpeg';
+import t2x2 from 'url:../../../../assets/photos/thumbnails2x/2.jpeg';
 
-import o3 from '../../../../assets/photos/big/3.jpeg';
-import t3 from '../../../../assets/photos/thumbnails/3.jpeg';
-import t2x3 from '../../../../assets/photos/thumbnails2x/3.jpeg';
+import o3 from 'url:../../../../assets/photos/big/3.jpeg';
+import t3 from 'url:../../../../assets/photos/thumbnails/3.jpeg';
+import t2x3 from 'url:../../../../assets/photos/thumbnails2x/3.jpeg';
 
-import o4 from '../../../../assets/photos/big/4.jpeg';
-import t4 from '../../../../assets/photos/thumbnails/4.jpeg';
-import t2x4 from '../../../../assets/photos/thumbnails2x/4.jpeg';
+import o4 from 'url:../../../../assets/photos/big/4.jpeg';
+import t4 from 'url:../../../../assets/photos/thumbnails/4.jpeg';
+import t2x4 from 'url:../../../../assets/photos/thumbnails2x/4.jpeg';
 
-import o5 from '../../../../assets/photos/big/5.jpeg';
-import t5 from '../../../../assets/photos/thumbnails/5.jpeg';
-import t2x5 from '../../../../assets/photos/thumbnails2x/5.jpeg';
+import o5 from 'url:../../../../assets/photos/big/5.jpeg';
+import t5 from 'url:../../../../assets/photos/thumbnails/5.jpeg';
+import t2x5 from 'url:../../../../assets/photos/thumbnails2x/5.jpeg';
 
-import o6 from '../../../../assets/photos/big/6.jpeg';
-import t6 from '../../../../assets/photos/thumbnails/6.jpeg';
-import t2x6 from '../../../../assets/photos/thumbnails2x/6.jpeg';
+import o6 from 'url:../../../../assets/photos/big/6.jpeg';
+import t6 from 'url:../../../../assets/photos/thumbnails/6.jpeg';
+import t2x6 from 'url:../../../../assets/photos/thumbnails2x/6.jpeg';
 
-import o7 from '../../../../assets/photos/big/7.jpeg';
-import t7 from '../../../../assets/photos/thumbnails/7.jpeg';
-import t2x7 from '../../../../assets/photos/thumbnails2x/7.jpeg';
+import o7 from 'url:../../../../assets/photos/big/7.jpeg';
+import t7 from 'url:../../../../assets/photos/thumbnails/7.jpeg';
+import t2x7 from 'url:../../../../assets/photos/thumbnails2x/7.jpeg';
 
-import o8 from '../../../../assets/photos/big/8.jpeg';
-import t8 from '../../../../assets/photos/thumbnails/8.jpeg';
-import t2x8 from '../../../../assets/photos/thumbnails2x/8.jpeg';
+import o8 from 'url:../../../../assets/photos/big/8.jpeg';
+import t8 from 'url:../../../../assets/photos/thumbnails/8.jpeg';
+import t2x8 from 'url:../../../../assets/photos/thumbnails2x/8.jpeg';
 
-import o9 from '../../../../assets/photos/big/9.jpeg';
-import t9 from '../../../../assets/photos/thumbnails/9.jpeg';
-import t2x9 from '../../../../assets/photos/thumbnails2x/9.jpeg';
+import o9 from 'url:../../../../assets/photos/big/9.jpeg';
+import t9 from 'url:../../../../assets/photos/thumbnails/9.jpeg';
+import t2x9 from 'url:../../../../assets/photos/thumbnails2x/9.jpeg';
 
-import o10 from '../../../../assets/photos/big/10.jpeg';
-import t10 from '../../../../assets/photos/thumbnails/10.jpeg';
-import t2x10 from '../../../../assets/photos/thumbnails2x/10.jpeg';
+import o10 from 'url:../../../../assets/photos/big/10.jpeg';
+import t10 from 'url:../../../../assets/photos/thumbnails/10.jpeg';
+import t2x10 from 'url:../../../../assets/photos/thumbnails2x/10.jpeg';
 
-import o11 from '../../../../assets/photos/big/11.jpeg';
-import t11 from '../../../../assets/photos/thumbnails/11.jpeg';
-import t2x11 from '../../../../assets/photos/thumbnails2x/11.jpeg';
+import o11 from 'url:../../../../assets/photos/big/11.jpeg';
+import t11 from 'url:../../../../assets/photos/thumbnails/11.jpeg';
+import t2x11 from 'url:../../../../assets/photos/thumbnails2x/11.jpeg';
 
-import o12 from '../../../../assets/photos/big/12.jpeg';
-import t12 from '../../../../assets/photos/thumbnails/12.jpeg';
-import t2x12 from '../../../../assets/photos/thumbnails2x/12.jpeg';
+import o12 from 'url:../../../../assets/photos/big/12.jpeg';
+import t12 from 'url:../../../../assets/photos/thumbnails/12.jpeg';
+import t2x12 from 'url:../../../../assets/photos/thumbnails2x/12.jpeg';
 
-import o13 from '../../../../assets/photos/big/13.jpeg';
-import t13 from '../../../../assets/photos/thumbnails/13.jpeg';
-import t2x13 from '../../../../assets/photos/thumbnails2x/13.jpeg';
+import o13 from 'url:../../../../assets/photos/big/13.jpeg';
+import t13 from 'url:../../../../assets/photos/thumbnails/13.jpeg';
+import t2x13 from 'url:../../../../assets/photos/thumbnails2x/13.jpeg';
 
-import o14 from '../../../../assets/photos/big/14.jpeg';
-import t14 from '../../../../assets/photos/thumbnails/14.jpeg';
-import t2x14 from '../../../../assets/photos/thumbnails2x/14.jpeg';
+import o14 from 'url:../../../../assets/photos/big/14.jpeg';
+import t14 from 'url:../../../../assets/photos/thumbnails/14.jpeg';
+import t2x14 from 'url:../../../../assets/photos/thumbnails2x/14.jpeg';
 
-import o15 from '../../../../assets/photos/big/15.jpeg';
-import t15 from '../../../../assets/photos/thumbnails/15.jpeg';
-import t2x15 from '../../../../assets/photos/thumbnails2x/15.jpeg';
+import o15 from 'url:../../../../assets/photos/big/15.jpeg';
+import t15 from 'url:../../../../assets/photos/thumbnails/15.jpeg';
+import t2x15 from 'url:../../../../assets/photos/thumbnails2x/15.jpeg';
 
-import o16 from '../../../../assets/photos/big/16.jpeg';
-import t16 from '../../../../assets/photos/thumbnails/16.jpeg';
-import t2x16 from '../../../../assets/photos/thumbnails2x/16.jpeg';
+import o16 from 'url:../../../../assets/photos/big/16.jpeg';
+import t16 from 'url:../../../../assets/photos/thumbnails/16.jpeg';
+import t2x16 from 'url:../../../../assets/photos/thumbnails2x/16.jpeg';
 
-import o17 from '../../../../assets/photos/big/17.jpeg';
-import t17 from '../../../../assets/photos/thumbnails/17.jpeg';
-import t2x17 from '../../../../assets/photos/thumbnails2x/17.jpeg';
+import o17 from 'url:../../../../assets/photos/big/17.jpeg';
+import t17 from 'url:../../../../assets/photos/thumbnails/17.jpeg';
+import t2x17 from 'url:../../../../assets/photos/thumbnails2x/17.jpeg';
 
-import o18 from '../../../../assets/photos/big/18.jpeg';
-import t18 from '../../../../assets/photos/thumbnails/18.jpeg';
-import t2x18 from '../../../../assets/photos/thumbnails2x/18.jpeg';
+import o18 from 'url:../../../../assets/photos/big/18.jpeg';
+import t18 from 'url:../../../../assets/photos/thumbnails/18.jpeg';
+import t2x18 from 'url:../../../../assets/photos/thumbnails2x/18.jpeg';
 
-import o19 from '../../../../assets/photos/big/19.jpeg';
-import t19 from '../../../../assets/photos/thumbnails/19.jpeg';
-import t2x19 from '../../../../assets/photos/thumbnails2x/19.jpeg';
+import o19 from 'url:../../../../assets/photos/big/19.jpeg';
+import t19 from 'url:../../../../assets/photos/thumbnails/19.jpeg';
+import t2x19 from 'url:../../../../assets/photos/thumbnails2x/19.jpeg';
 
-import o20 from '../../../../assets/photos/big/20.jpeg';
-import t20 from '../../../../assets/photos/thumbnails/20.jpeg';
-import t2x20 from '../../../../assets/photos/thumbnails2x/20.jpeg';
+import o20 from 'url:../../../../assets/photos/big/20.jpeg';
+import t20 from 'url:../../../../assets/photos/thumbnails/20.jpeg';
+import t2x20 from 'url:../../../../assets/photos/thumbnails2x/20.jpeg';
 
-import o21 from '../../../../assets/photos/big/21.jpeg';
-import t21 from '../../../../assets/photos/thumbnails/21.jpeg';
-import t2x21 from '../../../../assets/photos/thumbnails2x/21.jpeg';
+import o21 from 'url:../../../../assets/photos/big/21.jpeg';
+import t21 from 'url:../../../../assets/photos/thumbnails/21.jpeg';
+import t2x21 from 'url:../../../../assets/photos/thumbnails2x/21.jpeg';
 
-import o22 from '../../../../assets/photos/big/22.jpeg';
-import t22 from '../../../../assets/photos/thumbnails/22.jpeg';
-import t2x22 from '../../../../assets/photos/thumbnails2x/22.jpeg';
+import o22 from 'url:../../../../assets/photos/big/22.jpeg';
+import t22 from 'url:../../../../assets/photos/thumbnails/22.jpeg';
+import t2x22 from 'url:../../../../assets/photos/thumbnails2x/22.jpeg';
 
-import o23 from '../../../../assets/photos/big/23.jpeg';
-import t23 from '../../../../assets/photos/thumbnails/23.jpeg';
-import t2x23 from '../../../../assets/photos/thumbnails2x/23.jpeg';
+import o23 from 'url:../../../../assets/photos/big/23.jpeg';
+import t23 from 'url:../../../../assets/photos/thumbnails/23.jpeg';
+import t2x23 from 'url:../../../../assets/photos/thumbnails2x/23.jpeg';
 
-import o24 from '../../../../assets/photos/big/24.jpeg';
-import t24 from '../../../../assets/photos/thumbnails/24.jpeg';
-import t2x24 from '../../../../assets/photos/thumbnails2x/24.jpeg';
+import o24 from 'url:../../../../assets/photos/big/24.jpeg';
+import t24 from 'url:../../../../assets/photos/thumbnails/24.jpeg';
+import t2x24 from 'url:../../../../assets/photos/thumbnails2x/24.jpeg';
 
-import o25 from '../../../../assets/photos/big/25.jpeg';
-import t25 from '../../../../assets/photos/thumbnails/25.jpeg';
-import t2x25 from '../../../../assets/photos/thumbnails2x/25.jpeg';
+import o25 from 'url:../../../../assets/photos/big/25.jpeg';
+import t25 from 'url:../../../../assets/photos/thumbnails/25.jpeg';
+import t2x25 from 'url:../../../../assets/photos/thumbnails2x/25.jpeg';
 
-import o26 from '../../../../assets/photos/big/26.jpeg';
-import t26 from '../../../../assets/photos/thumbnails/26.jpeg';
-import t2x26 from '../../../../assets/photos/thumbnails2x/26.jpeg';
+import o26 from 'url:../../../../assets/photos/big/26.jpeg';
+import t26 from 'url:../../../../assets/photos/thumbnails/26.jpeg';
+import t2x26 from 'url:../../../../assets/photos/thumbnails2x/26.jpeg';
 
-import o27 from '../../../../assets/photos/big/27.jpeg';
-import t27 from '../../../../assets/photos/thumbnails/27.jpeg';
-import t2x27 from '../../../../assets/photos/thumbnails2x/27.jpeg';
+import o27 from 'url:../../../../assets/photos/big/27.jpeg';
+import t27 from 'url:../../../../assets/photos/thumbnails/27.jpeg';
+import t2x27 from 'url:../../../../assets/photos/thumbnails2x/27.jpeg';
 
-import o28 from '../../../../assets/photos/big/28.jpeg';
-import t28 from '../../../../assets/photos/thumbnails/28.jpeg';
-import t2x28 from '../../../../assets/photos/thumbnails2x/28.jpeg';
+import o28 from 'url:../../../../assets/photos/big/28.jpeg';
+import t28 from 'url:../../../../assets/photos/thumbnails/28.jpeg';
+import t2x28 from 'url:../../../../assets/photos/thumbnails2x/28.jpeg';
 
-import o29 from '../../../../assets/photos/big/29.jpeg';
-import t29 from '../../../../assets/photos/thumbnails/29.jpeg';
-import t2x29 from '../../../../assets/photos/thumbnails2x/29.jpeg';
+import o29 from 'url:../../../../assets/photos/big/29.jpeg';
+import t29 from 'url:../../../../assets/photos/thumbnails/29.jpeg';
+import t2x29 from 'url:../../../../assets/photos/thumbnails2x/29.jpeg';
 
-import o30 from '../../../../assets/photos/big/30.jpeg';
-import t30 from '../../../../assets/photos/thumbnails/30.jpeg';
-import t2x30 from '../../../../assets/photos/thumbnails2x/30.jpeg';
+import o30 from 'url:../../../../assets/photos/big/30.jpeg';
+import t30 from 'url:../../../../assets/photos/thumbnails/30.jpeg';
+import t2x30 from 'url:../../../../assets/photos/thumbnails2x/30.jpeg';
 
-import o31 from '../../../../assets/photos/big/31.jpeg';
-import t31 from '../../../../assets/photos/thumbnails/31.jpeg';
-import t2x31 from '../../../../assets/photos/thumbnails2x/31.jpeg';
+import o31 from 'url:../../../../assets/photos/big/31.jpeg';
+import t31 from 'url:../../../../assets/photos/thumbnails/31.jpeg';
+import t2x31 from 'url:../../../../assets/photos/thumbnails2x/31.jpeg';
 
-import o32 from '../../../../assets/photos/big/32.jpeg';
-import t32 from '../../../../assets/photos/thumbnails/32.jpeg';
-import t2x32 from '../../../../assets/photos/thumbnails2x/32.jpeg';
+import o32 from 'url:../../../../assets/photos/big/32.jpeg';
+import t32 from 'url:../../../../assets/photos/thumbnails/32.jpeg';
+import t2x32 from 'url:../../../../assets/photos/thumbnails2x/32.jpeg';
 
 
 interface Photo {
